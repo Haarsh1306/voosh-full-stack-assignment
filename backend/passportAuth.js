4fc322e37c44a07adb064cc687b50e52e569198e
Harsh Verma <hv41096@gmail.com>
2024-09-08 22:36:20 +0530

    URL update

--- backend/passportAuth.js
+++ backend/passportAuth.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/api/auth/google/callback",
+      callbackURL: "https://voosh-full-stack-assignment.onrender.com/api/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, cb) {
       const email = profile.emails[0].value;
